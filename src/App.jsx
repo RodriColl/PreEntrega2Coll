b43f8974f0840c07dd5968da69d54ea9eb47ebe2
rodrigo <rodri_coll@hotmail.com>
2024-10-23 19:48:53 -0300

    Inicio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import NavBar from './components/navBar';
+
+const App = () => {
+    return (
+        <div>
+            <NavBar />
+            <h1>DiTech</h1>
+        </div>
+    );
+};
+
+export default App;
