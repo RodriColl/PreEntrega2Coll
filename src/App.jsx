c44e56f358671d22031a3a98aa67fef9d18d394f
Rodrigo Coll <rodri_coll@hotmail.com>
2024-11-11 11:13:32 -0300

    Estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 
 function App() {
     return (
-        <Router>
+        <BrowserRouter>
             <NavBar />
             <Routes>
                 <Route path="/" element={<ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />} />
                 <Route path="/category/:id" element={<ItemListContainer />} /> 
                 <Route path="/item/:id" element={<ItemDetailContainer />} />
             </Routes>
-        </Router>
+        </BrowserRouter>
     );
 }
 
